Fixed log_step calculation.

trainer/trainer.py
@@ -24,7 +24,7 @@ class Trainer(BaseTrainer):
         self.valid_data_loader = valid_data_loader
         self.do_validation = self.valid_data_loader is not None
         self.lr_scheduler = lr_scheduler
-        self.log_step = int(np.sqrt(data_loader.batch_size))
+        self.log_step = int(np.sqrt(len(data_loader)))
 
         self.train_metrics = MetricTracker(writer=self.writer)
         self.valid_metrics = MetricTracker()